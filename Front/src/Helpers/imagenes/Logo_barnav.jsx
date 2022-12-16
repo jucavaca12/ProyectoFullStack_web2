1147966d2ce135370000e105fe3d7bbefe6f859c
juanlopez9117 <estebanmurillo971@gmail.com>
2022-12-16 10:29:42 -0500

    se dan estilos al header y se corrigen errores

--- Front/src/Helpers/imagenes/Logo_barnav.jsx
+++ Front/src/Helpers/imagenes/Logo_barnav.jsx
@@ -3,7 +3,7 @@ import logo_navbar from '../../../public/logo_navbar.jpg';
 const logoHeader = ( )=>{
     return(
         <section>
-
+            <img className='fondo' src={logo_navbar} alt="logo_navbar" />
         </section>
     )
 }
